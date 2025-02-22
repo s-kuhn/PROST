e9ea2abd3e586927f34239a1540bfcf4aed7d0bd
s-kuhn <dev+github@s-kuhn.de>
2025-02-22 02:45:58 +0100

    rename groupId

--- server/src/main/kotlin/de/s_kuhn/prost/ProstApplication.kt
+++ server/src/main/kotlin/de/s_kuhn/prost/ProstApplication.kt
@@ -1,4 +1,4 @@
-package edu.s_kuhn.prost
+package de.s_kuhn.prost
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- server/src/test/kotlin/de/s_kuhn/prost/ProstApplicationTests.kt
+++ server/src/test/kotlin/de/s_kuhn/prost/ProstApplicationTests.kt
@@ -1,4 +1,4 @@
-package edu.s_kuhn.prost
+package de.s_kuhn.prost
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
