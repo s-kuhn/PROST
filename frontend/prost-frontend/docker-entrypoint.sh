af19059f6d32baadc403588271859f2c0fb4ca2c
s-kuhn <dev+github@s-kuhn.de>
2025-07-27 02:35:00 +0200

    update pipeline

--- frontend/prost-frontend/docker-entrypoint.sh
+++ frontend/prost-frontend/docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 # Set default values if not provided
-export BACKEND_HOST=${BACKEND_HOST:-backend}
-export BACKEND_PORT=${BACKEND_PORT:-8080}
-export KEYCLOAK_HOST=${KEYCLOAK_HOST:-keycloak}
-export KEYCLOAK_PORT=${KEYCLOAK_PORT:-8081}
+export BACKEND_HOST=${BACKEND_HOST:-backend:8080}
+export KEYCLOAK_HOST=${KEYCLOAK_HOST:-keycloak:8081}
 
 # Replace environment variables in built Angular files
 find /usr/share/nginx/html -name "*.js" -exec sed -i "s|__API_URL__|${API_URL}|g" {} \;
@@ -13,11 +11,11 @@ find /usr/share/nginx/html -name "*.js" -exec sed -i "s|__KEYCLOAK_REALM__|${KEY
 find /usr/share/nginx/html -name "*.js" -exec sed -i "s|__KEYCLOAK_CLIENT_ID__|${KEYCLOAK_CLIENT_ID}|g" {} \;
 
 # Generate nginx.conf from template
-envsubst '${BACKEND_HOST} ${BACKEND_PORT} ${KEYCLOAK_HOST} ${KEYCLOAK_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+envsubst '${BACKEND_HOST} ${KEYCLOAK_HOST}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 echo "Generated nginx.conf with:"
-echo "  BACKEND_HOST=${BACKEND_HOST}:${BACKEND_PORT}"
-echo "  KEYCLOAK_HOST=${KEYCLOAK_HOST}:${KEYCLOAK_PORT}"
+echo "  BACKEND_HOST=${BACKEND_HOST}"
+echo "  KEYCLOAK_HOST=${KEYCLOAK_HOST}"
 echo "Environment variables replaced in Angular build"
 
 # Start nginx
